60e3e0833542283fd568baafd99bc91d1a798870
Sarthak Joshi <sarthaksj02@gmail.com>
2025-03-31 10:08:17 +0530

    fix: update test schema

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv"
 import { Request, Response } from "express"
 dotenv.config()
 
-const signup = async (req: Request, res: Response) => {}
+const signup = async (req: Request, res: Response) => {
+    
+}
 const login = async (req: Request, res: Response) => {}
 
 export { signup, login }
--- src/models/testModel.ts
+++ src/models/testModel.ts
@@ -14,6 +14,11 @@ interface Question {
 	correctOptionIndex: number
 }
 
+interface Result {
+	studentId: string
+	result: number
+}
+
 const questionSchema = new mongoose.Schema<Question>({
 	questionText: { type: String, required: true },
 	questionNumber: { type: Number, required: true },
@@ -31,11 +36,20 @@ const questionSchema = new mongoose.Schema<Question>({
 	correctOptionIndex: { type: Number, required: true },
 })
 
+const ResultSchema = new mongoose.Schema<Result>({
+	studentId: { type: String, required: true },
+	result: { type: Number, required: true },
+})
+
 const testSchema = new mongoose.Schema({
 	testName: { type: String, required: true },
 	questions: { type: [questionSchema], required: true },
-	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
+	postedAt: { type: Date, default: Date.now },
+	startFrom: { type: Date, required: true },
+	deadlineTime: { type: Date, required: true },
+	testTime: { type: Number, required: true },
+	result: { type: [ResultSchema], required: true },
+	classroomId: { type: String, required: true },
 })
 
 export const User = mongoose.models?.Test || mongoose.model("User", testSchema)
