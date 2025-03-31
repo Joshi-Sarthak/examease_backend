b617681d3f83caa870e66224930b9e024ecc0f70
Sarthak Joshi <sarthaksj02@gmail.com>
2025-03-31 09:07:14 +0530

    feat: connect to mongo and add structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import { authRoutes } from "./routes/authRoutes.js"
+import { userRoutes } from "./routes/userRoutes.js"
+import { classroomRoutes } from "./routes/classroomRoutes.js"
+import { testRoutes } from "./routes/testRoutes.js"
+import mongoose from "mongoose"
+
+dotenv.config()
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+	cors({
+		credentials: true,
+		origin: ["http://localhost:4200"],
+	})
+)
 
 app.get("/", (req, res) => {
-	res.send("Hello World! yo")
+	res.json({ msg: "OK" })
 })
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+app.use("/auth", authRoutes)
+app.use("/user", userRoutes)
+app.use("/classroom", classroomRoutes)
+app.use("/test", testRoutes)
+
+mongoose
+	.connect(process.env.MONGO_URI!)
+	.then((res) => {
+		console.log("connected to DB")
+		app.listen(PORT, () => {
+			console.log("listening on port ", PORT)
+		})
+	})
+	.catch((err) => {
+		console.log(err)
+	})
