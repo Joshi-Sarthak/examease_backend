9741e5224bab589ea62e62c02f39d82689ee3bf7
harsh <harshitkar104@gmail.com>
2025-04-04 20:44:43 +0530

    fix: updated edit user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,20 +3,19 @@ import { User } from "../models/userModel.js"
 
 export const editUser = async (req: Request, res: Response): Promise<any> => {
 	try {
-		const userId = req.body
-		const updates = req.body
+		const { userId, ...updates } = req.body;
 
-		const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true })
+		const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true });
 
 		if (!updatedUser) {
-			return res.status(404).json({ error: "User not found" })
+			return res.status(404).json({ error: "User not found" });
 		}
 
-		res.json({ message: "User updated successfully", user: updatedUser })
+		res.json({ message: "User updated successfully", user: updatedUser });
 	} catch (error) {
-		res.status(500).json({ error: "Internal server error" })
+		res.status(500).json({ error: "Internal server error" });
 	}
-}
+};
 
 export const deleteUser = async (req: Request, res: Response): Promise<any> => {
 	try {
